c7a1a660fc2c708fb236e1e9492fbda5e071909b
sivakumarss <sivakumarss@outlook.com>
2024-07-11 18:15:47 +1000

    Add project files.

---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public async Task<User> AddUser(string firstName, string surname, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(surname))
+            {
+                throw new InvalidOperationException("user firstname / surname is required ");
+            }
+
+            if (!email.Contains("@") && !email.Contains("."))
+            {
+                throw new InvalidOperationException("user email is invalid ");
+            }
+
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                 throw new InvalidOperationException("user should be older than 21 years");
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = await clientRepository.GetByIdAsync(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                Firstname = firstName,
+                Surname = surname
+            };
+
+            if (client.Name == "VeryImportantClient")
+            {
+                // Skip credit check
+                user.HasCreditLimit = false;
+            }
+            else if (client.Name == "ImportantClient")
+            {
+                // Do credit check and double credit limit
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditServiceClient())
+                {
+                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                // Do credit check
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditServiceClient())
+                {
+                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                throw new InvalidOperationException("insufficient credit limit");
+            }
+
+            UserDataAccess.AddUser(user);
+
+            return user;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Refactoring test in C #
+
+## Description
+
+- You are asked to refactor the UserService class and more specifically its **AddUser** method.
+- You are asked to refactor API project as you see fit.
+- Assume that the code in the UserService class is sound in terms of business logic and only focus on applying clean code principles. Keep in mind acronyms such as SOLID, KISS, DRY and YAGNI.
+
+### **Task requirements**
+
+Please spend a maximum of 2 hours or as little time as you wish on the exercise.
+
+The following requirements have to be met.
+
+- Your code should compile and run in one step.
+- Feel free to use whatever libraries / packages you like.
+- Feel free to add additional class libs if you'd like to structure it differently.
+- You **must** include tests
+- Please avoid including artifacts from your local build (such as NuGet packages or the bin folder(s)) in your final ZIP file
+
+## Limitations
+
+You **can change anything** in the LegacyApp project except for the **UserDataAccess** class and its **AddUser** method. Both the class and the method NEED to stay static. You will fail this test if anything else is changed.
+
+## Technical questions**
+
+Please answer the following questions in a markdown file called `Answers to technical questions.md`.
+
+- How long did you spend on the coding test? What would you add to your solution if you had more time? If you didn't spend much time on the coding test then use this as an opportunity to explain what you would add.
+- What was the most useful feature that was added to the latest version of C#? Please include a snippet of code that shows how you've used it.
+- How would you track down an issue with this in production, assuming this api would be deployed as an app service in Azure.
